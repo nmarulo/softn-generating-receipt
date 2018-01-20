d71eeae9cc8765f4baae713da97f9fbe1a233da4
Nicolás Marulanda P <nicomaru@outlook.com>
2018-01-20 20:30:21 +0100

    Script, generar factura y mensajes - [Modificado] [Script:script-generating] [Función:generatingReceipt] Ahora al llamar a la función "generateAndGetLastReceipt" se incluyen los mensajes, se deshabilitan los campos del formulario y se borran los datos de los productos, luego se comprueba si la respuesta la llamada de "/generating" retorno "false", en ese caso no se generara el PDF. - [Modificado] [Clase:GeneratingController] [Método:generate] Ahora retornara false en caso de error. - [Nuevo] [Clase:GeneratingController] [Métodos:insertProducts] Guarda los productos de la factura, en caso de error borra las relaciones. - [Nuevo] [Clase:GeneratingController] Método "deleteReceiptsProducts". - [Modificado] [Clase:GeneratingController] [Método:updateClient] Ahora retorna un valor boolean. - [Nuevo] [Clase:GeneratingController] [Método:productsClients] Inserta los productos y actualiza el cliente, en caso de que al actualizar el cliente falle se borraran los productos. Retorna un valor boolean. - [Modificado] [Clase:GeneratingController] [Método:insertReceipt] Ahora en el caso de fallar al insertar los productos o actualizar el cliente, se borrara la factura y retornara false, de igual forma retornara false y falla al insertar la factura.

--- App/Controllers/GeneratingController.php
+++ App/Controllers/GeneratingController.php
@@ -92,18 +92,16 @@ class GeneratingController extends Controller {
             return json_encode(FALSE);
         }
         
-        $result = ['receipt_id' => 0];
-        
-        if ($receipt = $this->insertReceipt($request)) {
-            $this->insertProducts($receipt);
-            $this->updateClient($receipt->client_id);
-            $result['receipt_id'] = $receipt->id;
+        if ($receipt = $this->insertReceipt($request, $products)) {
+            Messages::addSuccess('Factura creada correctamente.');
+            
+            return json_encode(['receipt_id' => $receipt->id]);
         }
         
-        return json_encode($result);
+        return json_encode(FALSE);
     }
     
-    private function insertReceipt(Request $request) {
+    private function insertReceipt(Request $request, $products) {
         $receipt                        = new Receipts();
         $receipt->receipt_type          = $request->input('receipt_type');
         $receipt->receipt_number        = $request->input('receipt_number');
@@ -111,33 +109,89 @@ class GeneratingController extends Controller {
         $receipt->receipt_license_plate = $request->input('receipt_license_plate');
         $receipt->client_id             = $request->input('client_id');
         
-        if (!$receipt = $receipt->save()) {
-            Messages::addDanger('Error al registrar la factura.');
+        try {
+            if (!($receipt = $receipt->save()) || !$this->productsClients($products, $receipt)) {
+                $receipt->delete();
+                $receipt = FALSE;
+                Messages::addDanger('Error al registrar la factura.');
+            }
+        } catch (\Exception $ex) {
+            $receipt = FALSE;
+            //TODO: log
+            Messages::addDanger('* Error al registrar la factura.');
         }
         
         return $receipt;
     }
     
-    private function insertProducts(Receipts $receipt) {
+    private function productsClients($products, Receipts $receipt) {
+        $result = $this->insertProducts($products, $receipt);
+        
+        if ($result && !$this->updateClient($receipt->client_id)) {
+            $result = FALSE;
+            $this->deleteReceiptsProducts($products, $receipt->id, count($products));
+        }
+        
+        return $result;
+    }
+    
+    private function insertProducts($products, Receipts $receipt) {
+        $error                      = FALSE;
+        $len                        = count($products);
         $receiptProduct             = new ReceiptsProducts();
         $receiptProduct->receipt_id = $receipt->id;
+        $maxI                       = 0;
         
-        array_walk($products, function($product) use ($receiptProduct) {
-            $product                              = (array)$product;
-            $receiptProduct->product_id           = $product['id'];
-            $receiptProduct->receipt_product_unit = $product['receipt_product_unit'];
-            Query::insert(ReceiptsProducts::tableName(), $receiptProduct->data())
-                 ->execute();
-        });
+        try {
+            for ($i = 0; $i < $len && !$error; ++$i) {
+                $maxI                                 = $i;
+                $product                              = (array)$products[$i];
+                $receiptProduct->product_id           = $product['id'];
+                $receiptProduct->receipt_product_unit = $product['receipt_product_unit'];
+                $error                                = !Query::insert(ReceiptsProducts::tableName(), $receiptProduct->data())
+                                                              ->execute();
+            }
+        } catch (\Exception $ex) {
+            //TODO: log
+            $error = TRUE;
+            $this->deleteReceiptsProducts($products, $receipt->id, $maxI);
+        }
+        
+        if ($error) {
+            Messages::addDanger('Error al vincular los productos.');
+        }
+        
+        return !$error;
+    }
+    
+    private function deleteReceiptsProducts($products, $receiptId, $maxI) {
+        try {
+            for ($i = 0; $i < $maxI; ++$i) {
+                $product = (array)$products[$i];
+                Query::delete()
+                     ->from(ReceiptsProducts::tableName())
+                     ->where('product_id', $product['id'])
+                     ->where('receipt_id', $receiptId)
+                     ->execute();
+            }
+        } catch (\Exception $exception) {
+            //TODO: log.
+            Messages::addDanger('Error al borrar los productos vinculados.');
+        }
     }
     
     private function updateClient($clientId) {
         $client                         = Clients::find($clientId);
         $client->client_number_receipts = intval($client->client_number_receipts) + 1;
         
-        if(!$client->save()){
+        if (!$client->save()) {
+            //TODO: log
             Messages::addDanger('Error al actualizar el cliente.');
+            
+            return FALSE;
         }
+        
+        return TRUE;
     }
     
     public function dataModal(Request $request) {
