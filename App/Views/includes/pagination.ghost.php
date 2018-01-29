86fbafc7303817221af04cadf499fb4344a6f7d3
Nicolás Marulanda P <nicomaru@outlook.com>
2018-01-29 20:52:50 +0100

    Paginación - estilo

---
+++ App/Views/includes/pagination.ghost.php
@@ -0,0 +1,11 @@
+<div class="pagination-container">
+    <nav>
+        <ul class="pagination clearfix">
+            <li><a href="#"><span>&laquo;</span></a></li>
+            <li class="active"><a href="#">pagina 1</a></li>
+            <li><a href="#">pagina 2</a></li>
+            <li><a href="#">pagina 3</a></li>
+            <li><a href="#"><span>&raquo;</span></a></li>
+        </ul>
+    </nav>
+</div>
