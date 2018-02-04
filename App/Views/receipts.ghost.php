1896eb0e2dac336d772d50342210cc242689b56d
Nicolás Marulanda P <nicomaru@outlook.com>
2018-02-04 18:47:14 +0100

    icono

--- App/Views/receipts.ghost.php
+++ App/Views/receipts.ghost.php
@@ -27,7 +27,7 @@
                     <tr>
                         <td class="btn-actions">
                             <a class="btn btn-warning btn-generate-pdf" data-receipt-id="{{$receipt->id}}" href="{{url('/generating/datapdf')}}">
-                                <span class="glyphicon glyphicon-open-file"></span>
+                                <span class="glyphicon glyphicon-save-file"></span>
                             </a>
                             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-delete" data-element-id="{{$receipt->id}}" data-form-action="{{url('/receipts/delete')}}">
                                     <span class="glyphicon glyphicon-remove"></span>
